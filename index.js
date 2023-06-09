4b64c5df877468aab7591707274ccf9d20d36e11
hazratshahjalal <hazratshahjalal503@gmail.com>
2023-06-09 16:56:25 +0600

    await client connect removed

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+
 
     const classesCollection = client.db('LangLinkDB').collection('classes')
     const instructorsCollection = client.db('LangLinkDB').collection('instructors')
